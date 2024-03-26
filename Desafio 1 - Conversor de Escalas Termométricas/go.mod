92e189b1f6b6ff39c94f9c5ba0351b2613397dfa
PkMs7 <patrick.marques@zaffarinet.com.br>
2024-03-26 14:32:50 -0300

    Primeira versão do projeto de conversão de valores Kelvin para Celcius

---
+++ Desafio 1 - Conversor de Escalas Termométricas/go.mod
@@ -0,0 +1,3 @@
+module escalasTermometricas
+
+go 1.21.5
--- Desafio 1 - Conversor de Escalas Termométricas/main.go
+++ Desafio 1 - Conversor de Escalas Termométricas/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	escalas "escalasTermometricas/EscalasTermometricas"
 	"fmt"
 )
 
 func main() {
+	var valorEmKelvin float64 = 373.2
 
-	fmt.Println()
+	fmt.Printf("O valor convertido para Kelvin é: %.2f C°", escalas.ConversaoKelvinParaCelcius(valorEmKelvin))
 
 }
